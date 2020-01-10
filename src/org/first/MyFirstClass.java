e64bea4ab6434d6e7335fcc78a7ae718145f79f0
Maha3003 <vmahaas@gmail.com>
2020-01-10 09:03:25 +0530

    My First commit in github

---
+++ src/org/first/MyFirstClass.java
@@ -0,0 +1,16 @@
+package org.first;
+
+public class MyFirstClass {
+	
+	private void bePositive() {
+		// TODO Auto-generated method stub
+System.out.println("You can do it..Be positive");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyFirstClass mf = new MyFirstClass();
+		mf.bePositive();
+	}
+
+}
